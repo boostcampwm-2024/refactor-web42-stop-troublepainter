a4f52ea05a55ad9b88773e4fdfdabec07d2c95ac
AEU <aeuplayground@gmail.com>
2024-11-10 18:47:43 +0900

    feat: Add startDrawingEvent method and related functions
    you can draw a point

--- client/src/components/canvas/MainCanvas.tsx
+++ client/src/components/canvas/MainCanvas.tsx
@@ -1,11 +1,74 @@
-import { useRef } from 'react';
+import { useRef, TouchEvent, MouseEvent } from 'react';
+import { CANVAS_EVENT } from '@/constants/canvasConstants';
+import { useCanvasStore } from '@/stores/useCanvasStore';
+import { CanvasStore } from '@/types/canvas.types';
+
+const CANVAS_SIZE_WIDTH = 640; //임시 사이즈
+const CANVAS_SIZE_HEIGHT = 420;
+
+const getTouchPoint = (canvas: HTMLCanvasElement, e: TouchEvent<HTMLCanvasElement>) => {
+  const { clientX, clientY } = e.nativeEvent.touches[0]; //뷰포트 기준
+  const { top, left } = canvas.getBoundingClientRect(); // 캔버스의 뷰포트 기준 위치
+  return [clientX - left, clientY - top];
+};
+
+const getDrawPoint = (e: TouchEvent<HTMLCanvasElement> | MouseEvent<HTMLCanvasElement>, canvas: HTMLCanvasElement) => {
+  if (!canvas) new Error('canvas element가 없습니다.');
+
+  if (e.type === CANVAS_EVENT.MOUSE_DOWN || e.type === CANVAS_EVENT.MOUSE_MOVE) {
+    const event = e as MouseEvent<HTMLCanvasElement>;
+    return [event.nativeEvent.offsetX, event.nativeEvent.offsetY];
+  } else if (e.type === CANVAS_EVENT.TOUCH_DOWN || e.type === CANVAS_EVENT.TOUCH_MOVE) {
+    const event = e as TouchEvent<HTMLCanvasElement>;
+    return getTouchPoint(canvas, event);
+  } else {
+    throw new Error('mouse 혹은 touch 이벤트가 아닙니다.');
+  }
+};
 
 export const MainCanvas = () => {
   const mainCanvasRef = useRef<HTMLCanvasElement>(null);
+  const canDrawing = useCanvasStore((state: CanvasStore) => state.canDrawing);
+  const setCanDrawing = useCanvasStore((state: CanvasStore) => state.action.setCanDrawing);
+
+  const drawStartPath = (ctx: CanvasRenderingContext2D, drawX: number, drawY: number) => {
+    ctx.beginPath();
+
+    ctx.arc(drawX, drawY, 2, 0, Math.PI * 2);
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.moveTo(drawX, drawY);
+  };
+
+  const startDrawingEvent = (e: TouchEvent<HTMLCanvasElement> | MouseEvent<HTMLCanvasElement>) => {
+    if (canDrawing) return;
+    if (!mainCanvasRef.current) return;
+
+    const canvas = mainCanvasRef.current;
+    if (!canvas) return;
+
+    const ctx = canvas.getContext('2d');
+    if (!ctx) return;
+
+    try {
+      const [drawX, drawY] = getDrawPoint(e, canvas);
+      drawStartPath(ctx, drawX, drawY);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   return (
     <section>
-      <canvas className="touch-none border border-black" ref={mainCanvasRef}>
+      <canvas
+        className="touch-none border border-black"
+        ref={mainCanvasRef}
+        width={CANVAS_SIZE_WIDTH}
+        height={CANVAS_SIZE_HEIGHT}
+        onMouseDown={startDrawingEvent}
+        onTouchStart={startDrawingEvent}
+      >
         <img src="/" /> {/* canvas 지원하지 않는 브라우저일 경우 대체 이미지 */}
       </canvas>
     </section>
---
+++ client/src/constants/canvasConstants.ts
@@ -0,0 +1,6 @@
+export const CANVAS_EVENT = {
+  MOUSE_DOWN: 'mousedown',
+  MOUSE_MOVE: 'mousemove',
+  TOUCH_DOWN: 'touchstart',
+  TOUCH_MOVE: 'touchmove',
+};
