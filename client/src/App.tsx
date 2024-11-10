0afc809f9f38f8f83a120cc6f1cdb7921370f8ec
choiseona <tjsdk617@naver.com>
2024-11-10 21:12:41 +0900

    feat: Add auto-closing role assignment modal
    - Add modal UI for role display
    - Implement auto-close functionality after 3 seconds
    - Apply custom styling for role presentation
    - Include centered role text

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,19 @@
 import './App.css';
 import { useState } from 'react';
+import { Modal } from './components/ui/Modal';
+import { useModal } from './hooks/useModal';
 import helpIcon from '@/assets/help-icon.svg';
 import { Button } from '@/components/ui/Button';
 import { Logo } from '@/components/ui/Logo';
 import { UserInfoCard } from '@/components/ui/UserInfoCard';
 
-
 const App = () => {
   const [isReady, setIsReady] = useState(false);
-
+  const { isModalOpened, openModal } = useModal(3000);
   return (
     <>
       <Logo />
       <Logo variant="side" />
-
       <h1 className="transition">Hello world!</h1>
       <Button className="font-neodgm-pro text-2xl font-normal">유미라</Button>
       <Button variant="transperent" size="icon">
@@ -44,6 +44,12 @@ const App = () => {
           </div>
         </div>
       </div>
+
+      {/* Role 모달 */}
+      <button onClick={openModal}>3초 후 사라지는 Role 모달 오픈</button>
+      <Modal title="역할 배정" isModalOpened={isModalOpened} className="w-80">
+        <span className="flex min-h-28 items-center justify-center text-3xl text-violet-950">그림꾼</span>
+      </Modal>
     </>
   );
 };
