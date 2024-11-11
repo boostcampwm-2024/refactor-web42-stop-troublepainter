f703abd8b9f484fb315b940b2fb44803915964c7
AEU <aeuplayground@gmail.com>
2024-11-11 23:00:42 +0900

    feat: Add base resolution constants

---
+++ client/src/constants/canvasConstants.ts
@@ -0,0 +1,13 @@
+export const PENMODE = {
+  PEN: 0,
+  PAINTER: 1,
+};
+
+export const LINEWIDTH_VARIABLE = {
+  MIN_WIDTH: 2,
+  MAX_WIDTH: 10,
+  STEP_WIDTH: 2,
+};
+
+export const MAINCANVAS_RESOLUTION_WIDTH = 640;
+export const MAINCANVAS_RESOLUTION_HEIGHT = 420;
