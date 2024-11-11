f20feeb29dcdb2b58830700fd05b1fc6cc2b904d
rhino-ty <okko8522@gmail.com>
2024-11-11 19:29:01 +0900

    feat: reorganize router structure with layouts
    - Implement nested routing with RootLayout and GameLayout
    - Set up routes for main page and development pages

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,4 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom';
+import GameLayout from '@/layouts/GameLayout';
+import RootLayout from '@/layouts/RootLayout';
 import ExamplePage from '@/pages/ExamplePage';
 import MainPage from '@/pages/MainPage';
 // import WaitingRoomPage from '@/pages/WaitingRoomPage';
@@ -7,23 +9,38 @@ import MainPage from '@/pages/MainPage';
 
 export const router = createBrowserRouter([
   {
-    path: '/',
-    element: <MainPage />,
-  },
-  // {
-  //   path: '/room/:roomId',
-  //   element: <WaitingRoomPage />,
-  // },
-  // {
-  //   path: '/game/:roomId',
-  //   element: <GameRoomPage />,
-  // },
-  // {
-  //   path: '/result/:roomId',
-  //   element: <ResultPage />,
-  // },
-  {
-    path: '/dev',
-    element: <ExamplePage />,
+    element: <RootLayout />,
+    children: [
+      {
+        path: '/',
+        element: <MainPage />,
+      },
+      {
+        element: <GameLayout />,
+        children: [
+          {
+            path: '/devs',
+            element: <ExamplePage />,
+          },
+          // {
+          //   path: '/room/:roomId',
+          //   element: <WaitingRoomPage />,
+          // },
+          // {
+          //   path: '/game/:roomId',
+          //   element: <GameRoomPage />,
+          // },
+          // {
+          //   path: '/result/:roomId',
+          //   element: <ResultPage />,
+          // },
+        ],
+      },
+      // 개발용 페이지
+      {
+        path: '/dev',
+        element: <ExamplePage />,
+      },
+    ],
   },
 ]);
