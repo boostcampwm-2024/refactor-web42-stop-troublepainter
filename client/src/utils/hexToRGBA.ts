886ac0bd07ac88a38c1ce1b9dff3cb9f924241a0
AEU <aeuplayground@gmail.com>
2024-11-12 16:18:50 +0900

    comment: Write tsdoc about hexToRGBA.ts util

--- client/src/utils/hexToRGBA.ts
+++ client/src/utils/hexToRGBA.ts
@@ -1,3 +1,21 @@
+/**
+ * 16진수 색상 표기법(#00000)을 rgba 값을 담은 객체 구조로 반환해주는 함수입니다.
+ *
+ * @param hex - 16진수 색상 표기법을 string 타입으로 받습니다. (#000 등 축약형 사용 불가)
+ * @param alpha - 투명도입니다. 전달값이 없으면 자동으로 255가 채워집니다. (0 : 투명, 255 : 불투명)
+ * @returns RGBA 객체
+ *
+ * @example
+ * ```typescript
+ * // 투명한 검은색
+ * hexToRGBA('#00000', 0);
+ *
+ * // 불투명한 하얀색
+ * hexToRGBA('#ffffff', 0);
+ * ```
+ *
+ * @category Utils
+ */
 export function hexToRGBA(hex: string, alpha = 255) {
   hex = hex.replace('#', '');
 
