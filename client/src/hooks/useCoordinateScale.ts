e4859108dd73177752c096913520af23b498b581
AEU <aeuplayground@gmail.com>
2024-11-12 16:52:25 +0900

    style: Change resolution_width to camelCase

--- client/src/hooks/useCoordinateScale.ts
+++ client/src/hooks/useCoordinateScale.ts
@@ -1,18 +1,18 @@
 import { RefObject, useCallback, useEffect, useRef } from 'react';
 
-export const useCoordinateScale = (resolution_width: number, canvas: RefObject<HTMLCanvasElement>) => {
+export const useCoordinateScale = (resolutionWidth: number, canvas: RefObject<HTMLCanvasElement>) => {
   const coordinateScale = useRef(1);
   const resizeObserver = useRef<ResizeObserver | null>(null);
 
   const handleResizeCanvas = useCallback((entires: ResizeObserverEntry[]) => {
     const canvas = entires[0].target;
-    coordinateScale.current = resolution_width / canvas.getBoundingClientRect().width;
+    coordinateScale.current = resolutionWidth / canvas.getBoundingClientRect().width;
   }, []);
 
   useEffect(() => {
     if (!canvas.current) return;
 
-    coordinateScale.current = resolution_width / canvas.current.getBoundingClientRect().width;
+    coordinateScale.current = resolutionWidth / canvas.current.getBoundingClientRect().width;
     resizeObserver.current = new ResizeObserver(handleResizeCanvas);
     resizeObserver.current.observe(canvas.current);
 
