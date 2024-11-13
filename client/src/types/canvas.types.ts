9d0048e08abd331add31523c37e39c5ab0766112
AEU <aeuplayground@gmail.com>
2024-11-13 16:59:56 +0900

    feat: Add CanvasEventHandlers type

--- client/src/types/canvas.types.ts
+++ client/src/types/canvas.types.ts
@@ -1,3 +1,4 @@
+import { MouseEvent, TouchEvent } from 'react';
 import { PENMODE } from '@/constants/canvasConstants';
 
 interface PenOptions {
@@ -25,4 +26,17 @@ export interface RGBA {
   g: number;
   b: number;
   a: number;
+}
+
+//아래 태연님 코드 관련
+
+export interface CanvasEventHandlers {
+  onMouseDown?: (e: MouseEvent<HTMLCanvasElement>) => void;
+  onMouseMove?: (e: MouseEvent<HTMLCanvasElement>) => void;
+  onMouseUp?: (e: MouseEvent<HTMLCanvasElement>) => void;
+  onMouseLeave?: (e: MouseEvent<HTMLCanvasElement>) => void;
+  onTouchStart?: (e: TouchEvent<HTMLCanvasElement>) => void;
+  onTouchMove?: (e: TouchEvent<HTMLCanvasElement>) => void;
+  onTouchEnd?: (e: TouchEvent<HTMLCanvasElement>) => void;
+  onTouchCancle?: (e: TouchEvent<HTMLCanvasElement>) => void;
 }
