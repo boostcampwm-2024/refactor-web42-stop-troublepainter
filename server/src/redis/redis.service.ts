250968bbb30bb91098ac15553c815bef7702421d
ssum1ra <101113206+ssum1ra@users.noreply.github.com>
2024-11-14 01:30:19 +0900

    feat: Add Redis connection test

---
+++ server/src/redis/redis.service.ts
@@ -0,0 +1,29 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import Redis from 'ioredis';
+
+@Injectable()
+export class RedisService implements OnModuleInit, OnModuleDestroy {
+    private redisClient: Redis;
+
+    constructor(private configService: ConfigService) {
+        this.redisClient = new Redis({
+            host: this.configService.get<string>('REDIS_HOST'),
+            port: parseInt(this.configService.get<string>('REDIS_PORT'), 10),
+        });
+    }
+
+    async onModuleInit() {
+        console.log('Connecting to Redis...');
+        await this.redisClient.ping();
+        console.log('Connected to Redis!');
+    }
+
+    async onModuleDestroy() {
+        await this.redisClient.quit();
+    }
+
+    getClient() {
+        return this.redisClient;
+    }
+}
