95e519258767be7a319d25f87e495ab4c7b7f59f
rhino-ty <okko8522@gmail.com>
2024-11-14 17:20:20 +0900

    design: Adjust GameLayout height

--- client/src/layouts/GameLayout.tsx
+++ client/src/layouts/GameLayout.tsx
@@ -3,7 +3,7 @@ import { Logo } from '@/components/ui/Logo';
 
 const GameLayout = () => {
   return (
-    <div className="flex h-full flex-col justify-start bg-gradient-to-b from-violet-950 via-violet-800 to-fuchsia-800 lg:py-5">
+    <div className="flex min-h-screen flex-col justify-start bg-gradient-to-b from-violet-950 via-violet-800 to-fuchsia-800 lg:py-5">
       {/* 상단 헤더 */}
       <header className="flex items-center justify-center">
         <Logo variant="side" />
