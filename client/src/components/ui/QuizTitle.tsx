bf539c006c2a6ec767b6a8f40e2ec819b934c9f8
rhino-ty <okko8522@gmail.com>
2024-11-14 20:10:24 +0900

    design: Update QuizTitle component with consistent styling and timer positioning

--- client/src/components/ui/QuizTitle.tsx
+++ client/src/components/ui/QuizTitle.tsx
@@ -31,15 +31,15 @@ const QuizTitle = ({ className, currentRound, totalRound, remainingTime, title, 
         <h2 className="text-xl text-stroke-md lg:text-2xl xl:text-3xl">{title}</h2>
 
         {/* 타이머 */}
-        <div className="absolute -right-3 -top-5 w-[4.25rem] sm:-right-5 sm:-top-[1.3rem] sm:w-20 lg:-right-7 lg:-top-5 lg:w-20 xl:-right-[1.85rem] xl:-top-7 xl:w-24 2xl:-right-8 2xl:-top-9 2xl:w-28">
+        <div className="absolute -right-0 -top-[1.125rem] h-16 w-16 sm:-top-[1.3rem] sm:right-0 sm:h-20 sm:w-20 lg:-right-7 lg:-top-5 lg:w-20 xl:-right-[1.85rem] xl:-top-7 xl:w-24 2xl:-right-8 2xl:-top-9 2xl:w-28">
           <div className="relative">
             {remainingTime > 10 ? (
-              <img src={Timer} alt="타이머" className="h-full w-full" />
+              <img src={Timer} alt="타이머" className="object-fill" width={128} height={128} />
             ) : (
-              <img src={flashingTimer} alt="타이머" className="h-full w-full" />
+              <img src={flashingTimer} alt="타이머" className="object-fill" width={128} height={128} />
             )}
 
-            <span className="absolute inset-0 top-1/2 ml-[0.1rem] flex -translate-y-1/3 items-center justify-center text-base text-stroke-md sm:text-xl lg:ml-1 lg:text-2xl xl:text-3xl 2xl:text-[2rem]">
+            <span className="absolute inset-0 top-1/2 flex -translate-y-1/3 items-center justify-center text-base text-stroke-md sm:text-xl lg:ml-1 lg:text-2xl xl:text-3xl 2xl:text-[2rem]">
               {remainingTime}
             </span>
           </div>
