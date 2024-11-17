011276a3b01c128fb59977802c087df0c6e15c9f
ssum1ra <101113206+ssum1ra@users.noreply.github.com>
2024-11-17 22:10:32 +0900

    refactor: Add hgetall function

--- server/src/redis/redis.service.ts
+++ server/src/redis/redis.service.ts
@@ -17,14 +17,14 @@ export class RedisService {
         await this.redis.hset(key, value);
     }
 
-    async hget(key: string, field?: string): Promise<any> {
-        if (field) {
-            const value = await this.redis.hget(key, field);
-            return value;
-        } else {
-            const value = await this.redis.hgetall(key);
-            return Object.keys(value).length > 0 ? value : null;
-        }
+    async hgetall(key: string) {
+        const value = await this.redis.hgetall(key);
+        return Object.keys(value).length > 0 ? value : null;
+    }
+
+    async hget(key: string, field: string): Promise<any> {
+        const value = await this.redis.hget(key, field);
+        return value;
     }
 
     async del(key: string): Promise<void> {
