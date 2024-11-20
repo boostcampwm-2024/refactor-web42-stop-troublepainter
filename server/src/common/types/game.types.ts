4acfe0c95d8c48baba5e4df16b564738a4720fe3
ssum1ra <101113206+ssum1ra@users.noreply.github.com>
2024-11-20 11:42:46 +0900

    feat: Add WebSocket error handling

---
+++ server/src/common/types/game.types.ts
@@ -0,0 +1,26 @@
+import { PlayerRole, PlayerStatus, RoomStatus } from "../enums/game.status.enum";
+
+export interface Player {
+  playerId: string;
+  role: PlayerRole | null;
+  status: PlayerStatus;
+  nickname: string;
+  profileImage: string | null;
+  score: number;
+}
+
+export interface Room {
+  roomId: string;
+  hostId: string | null;
+  status: RoomStatus;
+  currentRound?: number;
+  totalRounds?: number;
+  currentWord?: string;
+}
+
+export interface RoomSettings {
+  maxPlayers: number;
+  totalRounds: number;
+  drawTime: number;
+}
+
