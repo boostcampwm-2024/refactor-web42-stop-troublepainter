f9ef90a62fffe29b231478ff1d1262a477e93357
choiseona <tjsdk617@naver.com>
2024-11-25 13:47:56 +0900

    feat: Add role-based modal display
    - Added myRole logic to fetch the current player's role
    - Opened role assignment modal when myRole is updated
    - Added condition to prevent rendering if any essential data (room, players, currentPlayerId, roomSettings, myRole) is missing
    - Displayed the role name dynamically using PLAYING_ROLE_TEXT

--- client/src/pages/GameRoomPage.tsx
+++ client/src/pages/GameRoomPage.tsx
@@ -1,15 +1,47 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { PlayerRole } from '@troublepainter/core';
 import { GameCanvas } from '@/components/canvas/GameCanvas';
+import { Modal } from '@/components/ui/Modal';
 import { QuizTitle } from '@/components/ui/QuizTitle';
+import { PLAYING_ROLE_TEXT } from '@/constants/gameConstant';
+import { useModal } from '@/hooks/useModal';
+import { useGameSocketStore } from '@/stores/socket/gameSocket.store';
 
 const GameRoomPage = () => {
   const [remainingTime] = useState(30);
+  const { isModalOpened, closeModal, handleKeyDown, openModal } = useModal(3000);
+  const { players, currentPlayerId, room, roomSettings } = useGameSocketStore();
+
+  const myRole = players.find((player) => player.playerId === currentPlayerId)?.role || null;
+
+  useEffect(() => {
+    if (myRole) openModal();
+  }, [myRole, room?.currentRound]);
+
+  if (!room || !players || !currentPlayerId || !roomSettings || !myRole) return null;
 
   return (
     <>
+      {/* 역할 모달 */}
+      <Modal
+        title="역할 배정"
+        isModalOpened={isModalOpened}
+        closeModal={closeModal}
+        handleKeyDown={handleKeyDown}
+        className="w-80"
+      >
+        <span className="flex min-h-28 items-center justify-center text-3xl text-violet-950">
+          {PLAYING_ROLE_TEXT[myRole]}
+        </span>
+      </Modal>
+
       {/* 중앙 영역 - 게임 화면 */}
-      <QuizTitle currentRound={1} totalRound={4} title="뭘까요?뭘까요?뭘까요?뭘까요?" remainingTime={remainingTime} />
+      <QuizTitle
+        currentRound={room.currentRound}
+        totalRound={roomSettings.totalRounds}
+        title="뭘까요?뭘까요?뭘까요?뭘까요?"
+        remainingTime={remainingTime}
+      />
       <GameCanvas role={PlayerRole.PAINTER} maxPixels={100000} />
     </>
   );
