c1c5924b51341f85ae55667f021cd234f3cf73dd
유미라 <yoomira@yoomira-MacBookAir.local>
2024-11-28 16:33:31 +0900

    feat: Implement winners array including painters

--- server/src/game/game.service.ts
+++ server/src/game/game.service.ts
@@ -266,14 +266,14 @@ export class GameService {
     await Promise.all(
       updatedPlayers.map((p) => this.gameRepository.updatePlayer(roomId, p.playerId, { score: p.score })),
     );
-
-    const winner = currentPlayer;
+    const painters = updatedPlayers.filter((p) => p.role === PlayerRole.PAINTER);
+    const winner = updatedPlayers.find((p) => p.playerId === playerId);
 
     return {
       isCorrect,
       roundNumber: room.currentRound,
       word: room.currentWord,
-      winner,
+      winners: [winner, ...painters],
       players: updatedPlayers,
     };
   }
@@ -324,7 +324,7 @@ export class GameService {
     return {
       roundNumber: room.currentRound,
       word: room.currentWord,
-      winner,
+      winners: [winner],
       players: updatedPlayers,
     };
   }
