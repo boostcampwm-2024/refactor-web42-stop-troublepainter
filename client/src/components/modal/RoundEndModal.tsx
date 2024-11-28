8326de6d9f51caaa050de0a5915d2d53039a8a24
rhino-ty <okko8522@gmail.com>
2024-11-28 13:59:42 +0900

    feat: Enhance RoundEndModal with Animation
    - Integrated DotLottie animation to visually indicate round win
    - Added autoplay functionality for the animation based on currentPlayerId matching roundWinner

--- client/src/components/modal/RoundEndModal.tsx
+++ client/src/components/modal/RoundEndModal.tsx
@@ -1,11 +1,13 @@
 import { useEffect } from 'react';
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 import { PlayerRole } from '@troublepainter/core';
 import { Modal } from '../ui/Modal';
+import roundWin from '@/assets/lottie/round-win.lottie';
 import { useModal } from '@/hooks/useModal';
 import { useGameSocketStore } from '@/stores/socket/gameSocket.store';
 
 const RoundEndModal = () => {
-  const { room, roundWinner, players, timers } = useGameSocketStore();
+  const { room, roundWinner, players, timers, currentPlayerId } = useGameSocketStore();
   const { isModalOpened, openModal, closeModal } = useModal();
 
   useEffect(() => {
@@ -19,29 +21,38 @@ const RoundEndModal = () => {
   const devil = players.find((player) => player.role === PlayerRole.DEVIL);
 
   return (
-    <Modal
-      title={room?.currentWord || ''}
-      isModalOpened={isModalOpened}
-      className="max-w-[26.875rem] sm:max-w-[61.75rem]"
-    >
-      <div className="flex min-h-[12rem] items-center justify-center sm:min-h-[15.75rem]">
-        <p className="text-center text-2xl sm:m-2 sm:text-3xl">
-          {roundWinner?.role === PlayerRole.DEVIL ? (
-            <> 정답을 맞춘 구경꾼이 없습니다</>
-          ) : (
-            <>
-              구경꾼 <span className="text-violet-600">{roundWinner?.nickname}</span>이 정답을 맞혔습니다
-            </>
-          )}
-        </p>
-      </div>
-      <div className="min-h-[4rem] rounded-md bg-violet-50 p-4 sm:m-2">
-        <p className="text-center text-xl text-violet-950 sm:text-2xl">
-          방해꾼은 <span className="text-violet-600">{devil?.nickname}</span>였습니다.
-        </p>
-        <span>{timers.ENDING}</span> {/* 임시 */}
-      </div>
-    </Modal>
+    <>
+      <DotLottieReact
+        src={roundWin}
+        autoplay={currentPlayerId === roundWinner?.playerId}
+        // autoplay={roundWinners.some((roundWinner) => currentPlayerId === roundWinner?.playerId)}
+        loop={false}
+        className="absolute left-1/2 top-1/2 z-50 h-screen w-full -translate-x-1/2 -translate-y-1/2"
+      />
+      <Modal
+        title={room?.currentWord || ''}
+        isModalOpened={isModalOpened}
+        className="max-w-[26.875rem] sm:max-w-[61.75rem]"
+      >
+        <div className="flex min-h-[12rem] items-center justify-center sm:min-h-[15.75rem]">
+          <p className="text-center text-2xl sm:m-2 sm:text-3xl">
+            {roundWinner?.role === PlayerRole.DEVIL ? (
+              <> 정답을 맞춘 구경꾼이 없습니다</>
+            ) : (
+              <>
+                구경꾼 <span className="text-violet-600">{roundWinner?.nickname}</span>이 정답을 맞혔습니다
+              </>
+            )}
+          </p>
+        </div>
+        <div className="min-h-[4rem] rounded-md bg-violet-50 p-4 sm:m-2">
+          <p className="text-center text-xl text-violet-950 sm:text-2xl">
+            방해꾼은 <span className="text-violet-600">{devil?.nickname}</span>였습니다.
+          </p>
+          <span>{timers.ENDING}</span> {/* 임시 */}
+        </div>
+      </Modal>
+    </>
   );
 };
 
