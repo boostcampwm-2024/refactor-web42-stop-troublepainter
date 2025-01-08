5c26a81ca187edb34f5bff716b0b4b0cb24e2e92
gimseonghwan <swkim12345@naver.com>
2025-01-08 23:39:37 +0900

    🧪test(chat.service.spec.ts): chat service 테스트 추가

--- server/src/chat/chat.service.spec.ts
+++ server/src/chat/chat.service.spec.ts
@@ -1,18 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ChatService } from './chat.service';
+import { ChatRepository } from './chat.repository';
+import { ModuleMocker } from 'jest-mock';
+
+const moduleMocker = new ModuleMocker(global);
 
 describe('ChatService', () => {
-  let service: ChatService;
+  let chatService: ChatService;
+  let chatRepository: ChatRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [ChatService],
     }).compile();
 
-    service = module.get<ChatService>(ChatService);
-  });
-
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+    chatService = module.get<ChatService>(ChatService);
   });
 });
