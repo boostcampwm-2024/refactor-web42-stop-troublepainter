4870ade053414cf4eedaf7e7ed8a193f7fbcbaf6
gimseonghwan <swkim12345@naver.com>
2025-01-24 00:00:59 +0900

    ♻️refactor(socket.config.ts): 클라이언트의 socket.io 설정을 websocket만 사용하게 변경

--- client/src/stores/socket/socket.config.ts
+++ client/src/stores/socket/socket.config.ts
@@ -82,6 +82,7 @@ export const SOCKET_CONFIG = {
     reconnection: true,
     reconnectionAttempts: 5,
     reconnectionDelay: 1000,
+    transports: ['websocket'],
   },
   /** 네임스페이스별 경로 */
   PATHS: {
