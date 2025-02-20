9b849ed1b358dfcbc0e6b7cd5d76086621e772d5
gimseonghwan <swkim12345@naver.com>
2025-02-20 20:16:36 +0900

    🏗️build: e2e test관련 의존성 설정

---
+++ e2e-test/playwright.config.ts
@@ -0,0 +1,22 @@
+import { devices, PlaywrightTestConfig } from '@playwright/test';
+
+const config: PlaywrightTestConfig = {
+  testDir: './crdt/test',
+  testMatch: '**/*.spec.ts',
+  workers: 5,
+  fullyParallel: true,
+  timeout: 180000,
+  reporter: 'html',
+  use: {
+    baseURL: 'http://localhost:5173/',
+    trace: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+};
+
+export default config;
